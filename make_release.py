ef88b7d6a094886545c0fc0b4dfe4efd2b3d8157
George Notaras <gnot@g-loaded.eu>
2012-05-11 18:39:30 +0300

    Updated release script.

--- make_release.py
+++ make_release.py
@@ -2,20 +2,50 @@
 # Copyright George Notaras
 
 REL_FILES = [
+    'AUTHORS',
 	'add-meta-tags.php',
-	'ChangeLog',
 	'LICENSE',
 	'NOTICE',
 	'README',
-	'THANKS',
+    'screenshot-1.png',
+    'uninstall.php',
+]
+
+REL_DIRS = [
+    'languages',
 ]
 
 PLUGIN_METADATA_FILE = 'add-meta-tags.php'
+
+POT_HEADER = """#  POT (Portable Object Template)
+#
+#  This file is part of the Creative-Commons-Configurator plugin for WordPress.
+#
+#  http://www.g-loaded.eu/2006/01/14/creative-commons-configurator-wordpress-plugin/
+#
+#  Copyright (C) 2006-2012 George Notaras <gnot@g-loaded.eu>
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+#
+"""
+
+# ==============================================================================
 
 import sys
 import os
 import zipfile
 import shutil
+import subprocess
 
 def get_name_release():
 	def get_data(cur_line):
@@ -44,19 +74,52 @@ def get_name_release():
 
 name, release = get_name_release()
 
+
+# Translation
+pot_domain = os.path.splitext(PLUGIN_METADATA_FILE)[0]
+
+# Generate POT file
+args = ['xgettext', '--default-domain=%s' % pot_domain, '--output=%s.pot' % pot_domain, '--language=PHP', '--from-code=UTF-8', '--keyword=__', '--keyword=_e', '--no-wrap', '--package-name=%s' % pot_domain, '--package-version=%s' % release, '--copyright-holder', 'George Notaras <gnot@g-loaded.eu>', '%s.php' % pot_domain]
+p = subprocess.Popen(args, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+stdout, stderr = p.communicate()
+
+# Replace POT Header
+
+f = open('%s.pot' % pot_domain, 'r')
+pot_lines = f.readlines()
+f.close()
+f = open('%s.pot' % pot_domain, 'w')
+f.write(POT_HEADER)
+for n, line in enumerate(pot_lines):
+    if n < 4:
+        continue
+    f.write(line)
+f.close()
+
+
 # Create release dir and move release files inside it
 os.mkdir(name)
+# Copy files
 for p_file in REL_FILES:
 	shutil.copy(p_file, os.path.join(name, p_file))
-
+# Copy dirs
+for p_dir in REL_DIRS:
+    shutil.copytree(p_dir, os.path.join(name, p_dir))
 
 # Create distribution package
 
 d_package_path = '%s-%s.zip' % (name, release)
 d_package = zipfile.ZipFile(d_package_path, 'w', zipfile.ZIP_DEFLATED)
 
+# Append root files
 for p_file in REL_FILES:
 	d_package.write(os.path.join(name, p_file))
+# Append language directory
+for p_dir in REL_DIRS:
+    d_package.write(os.path.join(name, p_dir))
+    # Append files in that directory
+    for p_file in os.listdir(os.path.join(name, p_dir)):
+        d_package.write(os.path.join(name, p_dir, p_file))
 
 d_package.testzip()
 
