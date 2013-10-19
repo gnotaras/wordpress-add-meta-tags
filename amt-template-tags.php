c0aa0916cb8d4a3a3451aacf9d615a0129bc18d6
George Notaras <gnot@g-loaded.eu>
2013-10-20 02:24:04 +0300

    Fixed template tags

--- amt-template-tags.php
+++ amt-template-tags.php
@@ -5,16 +5,19 @@
 
 
 function amt_content_description() {
-    echo amt_get_content_description();
+    $post = amt_get_current_post_object();
+    echo amt_get_content_description($post);
 }
 
 function amt_content_keywords() {
-    echo amt_get_content_keywords();
+    $post = amt_get_current_post_object();
+    echo amt_get_content_keywords($post);
 }
 
 function amt_content_keywords_mesh() {
+    $post = amt_get_current_post_object();
     // Keywords echoed in the form: keyword1;keyword2;keyword3
-    echo amt_get_content_keywords_mesh();
+    echo amt_get_content_keywords_mesh($post);
 }
 
 function amt_metadata() {
