712355a6d52047433ffa8338f6ab5194d9f67f80
George Notaras <gnot@g-loaded.eu>
2015-09-30 12:44:25 +0300

    Added support for title templates of archives of custom post types.

--- amt-utils.php
+++ amt-utils.php
@@ -2381,6 +2381,9 @@ function amt_get_title_for_title_element($options, $post) {
         // Author Archives
         'archive_author'        => '#entity_title# profile | #site_name#',
         'archive_author_paged'  => 'Content published by #entity_title# | Page #page# | #site_name#',
+        // Custom Post Type Archives
+        'archive_posttype_POSTTYPESLUG'        => '#entity_title# Archive | #site_name#',
+        'archive_posttype_POSTTYPESLUG_paged'  => '#entity_title# Archive | Page #page# | #site_name#',
         // Content
         // Content::Attachment
         'content_attachment'        => '#entity_title# | #site_name#',
@@ -2403,8 +2406,8 @@ function amt_get_title_for_title_element($options, $post) {
         //'content_post_quote'  => 'Quote: #entity_title# | #site_name#',
         //'content_post_chat'  => 'Chat: #entity_title# | #site_name#',
         // Content::Custom-Post-Type
-        'content_custom'        => '#entity_title# | #site_name#',
-        'content_custom_paged'  => '#entity_title# | Page #page# | #site_name#',
+        'content_POSTTYPESLUG'        => '#entity_title# | #site_name#',
+        'content_POSTTYPESLUG_paged'  => '#entity_title# | Page #page# | #site_name#',
         // is_error - TODO
         // is_search - TODO
     );
@@ -2456,6 +2459,9 @@ function amt_get_title_for_metadata($options, $post) {
         // Author Archives
         'archive_author'        => '#entity_title# profile',
         'archive_author_paged'  => 'Content published by #entity_title# | Page #page#',
+        // Custom Post Type Archives
+        //'archive_posttype_POSTTYPESLUG'        => '#entity_title# Archive',
+        //'archive_posttype_POSTTYPESLUG_paged'  => '#entity_title# Archive | Page #page#',
         // Content
         // Content::Attachment
         'content_attachment'        => '#entity_title#',
@@ -2478,8 +2484,8 @@ function amt_get_title_for_metadata($options, $post) {
         //'content_post_quote'  => 'Quote: #entity_title#',
         //'content_post_chat'  => 'Chat: #entity_title#',
         // Content::Custom-Post-Type
-        'content_custom'        => '#entity_title#',
-        'content_custom_paged'  => '#entity_title# | Page #page#',
+        'content_POSTTYPESLUG'        => '#entity_title#',
+        'content_POSTTYPESLUG_paged'  => '#entity_title# | Page #page#',
         // is_error - TODO
         // is_search - TODO
     );
@@ -2720,6 +2726,21 @@ function amt_internal_get_title($options, $post, $title_templates, $force_custom
                 $entity_title_template = $title_templates['archive_author_paged'];
             } elseif ( array_key_exists('archive_author', $title_templates) ) {
                 $entity_title_template = $title_templates['archive_author'];
+            }
+        }
+
+    // Custom Post Type Archive
+    } elseif ( is_post_type_archive() ) {
+        // Entity title
+        $entity_title = post_type_archive_title( $prefix='', $display=false );
+        // Entity title template
+        // $post is a content type object
+        $template_name = 'archive_posttype_' . $post->name;
+        if ( array_key_exists('enable_advanced_title_management', $options) && $options['enable_advanced_title_management'] == '1' ) {
+            if ( $page && $page >= 2 && array_key_exists($template_name . '_paged', $title_templates) ) {
+                $entity_title_template = $title_templates[$template_name . '_paged'];
+            } elseif ( array_key_exists($template_name, $title_templates) ) {
+                $entity_title_template = $title_templates[$template_name];
             }
         }
 
