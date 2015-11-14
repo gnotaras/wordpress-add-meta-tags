178d095386e10cca2ad9b8728dd5d2c9fa85a5d0
George Notaras <gnot@g-loaded.eu>
2015-11-14 17:39:57 +0200

    Major updates and refactoring.

--- amt-cli.php
+++ amt-cli.php
@@ -116,7 +116,6 @@ class AMT_Command extends WP_CLI_Command {
             }
         }
 
-
         // get_plugin_data( $plugin_file, $markup = true, $translate = true )
         //$plugin info = get_plugin_data( AMT_PLUGIN_DIR . 'add-meta-tags.php', $markup = true, $translate = true );
         // WP_CLI::line( ' ' );
@@ -131,7 +130,7 @@ class AMT_Command extends WP_CLI_Command {
      * 
      * ## EXAMPLES
      * 
-     *     wp amt upgrade
+     *     wp amt upgrade-setting
      *
      * @synopsis
      */
@@ -149,7 +148,7 @@ class AMT_Command extends WP_CLI_Command {
                 switch_to_blog( $blog['blog_id'] );
             }
             $plugin_info = get_plugin_data( plugin_dir_path( __FILE__ ) . 'add-meta-tags.php', $markup = true, $translate = true );
-            WP_CLI::line( 'Upgrading settings of ' . get_bloginfo('name') . ' - ' . $blog['blog_id'] );
+            WP_CLI::line( 'Upgrading settings of: ' . get_bloginfo('name') . ' - (ID: ' . $blog['blog_id'] . ')' );
             amt_plugin_upgrade();
             if ( is_multisite() ) {
                 restore_current_blog();
@@ -175,67 +174,246 @@ class AMT_Command extends WP_CLI_Command {
      * <what>
      * : The name of the person to greet.
      * 
-     * [--blog-id=<blog_id>]
-     * : The name of the person to greet.
-     * 
-     * [--blog-domain=<blog_domain>]
-     * : The name of the person to greet.
-     * 
      * ## EXAMPLES
      * 
-     *     wp amt export all
      *     wp amt export settings
      *     wp amt export postdata
      *     wp amt export userdata
      *
-     * @synopsis <what> [--blog-id=<id>]
+     * @synopsis <what>
      */
     function export( $args, $assoc_args ) {
         list( $what ) = $args;
 
-        // options:  --what=<all|settings|postdata|userdata> --blog-id ID
+        if ( ! in_array($what, array('settings', 'postdata', 'userdata')) ) {
+            WP_CLI::error( 'Invalid argument: ' . $what . ' (valid: settings|postdata|userdata)' );
+        }
+
+        $output = array();
+
+        // Export AMT settings
+        if ( $what == 'settings' ) {
+            $output = get_option("add_meta_tags_opts");
+            if ( empty($output) ) {
+                WP_CLI::error( 'Could not retrieve Add-Meta-Tags options.' );
+            }
+            //var_dump( $options );
+        }
+
+        // Export AMT custom fields
+        elseif ( $what == 'postdata' ) {
+            $qr_args = array(
+                'numberposts'       => -1,
+                'post_type'         => 'any',
+                'post_status'       => 'any',
+                'orderby'           => 'id',
+                'order'             => 'ASC',
+                'suppress_filters'  => true,
+            );
+            $posts_arr = get_posts( $qr_args );
+            $amt_post_fields = amt_get_post_custom_field_names();
+            foreach ( $posts_arr as $post ) {
+                foreach ( $amt_post_fields as $amt_post_field ) {
+                    $amt_post_field_value = get_post_meta( $post->ID, $amt_post_field, true );
+                    //var_dump($amt_field_value);
+                    if ( ! empty($amt_post_field_value) ) {
+                        // Export format: <post_id>;<amt_post_field>;<serialized_value>
+                        //echo json_encode( sprintf( '%s;%s;%s', $post->ID, $amt_field, $amt_field_value ) );
+                        $output[] = array($post->ID, $amt_post_field, $amt_post_field_value);
+                    }
+                }
+            }
+        }
+
+        // Export AMT contact infos
+        elseif ( $what == 'userdata' ) {
+            $qr_args = array(
+                'orderby'      => 'login',
+                'order'        => 'ASC',
+                'fields'       => 'all',
+            );
+            $users_arr = get_users( $qr_args );
+            $amt_user_fields = amt_get_user_contactinfo_field_names();
+            foreach ( $users_arr as $user ) {
+                foreach ( $amt_user_fields as $amt_user_field ) {
+                    $amt_user_field_value = get_the_author_meta( $amt_user_field, $user->ID );
+                    if ( ! empty($amt_user_field_value) ) {
+                        // Export format: <user_id>;<amt_user_field>;<serialized_value>
+                        //echo json_encode( sprintf( '%s;%s;%s', $post->ID, $amt_field, $amt_field_value ) );
+                        $output[] = array($user->ID, $amt_user_field, $amt_user_field_value);
+                    }
+                }
+            }
+        }
+
+        // Print output
+        if ( ! empty($output) ) {
+            echo json_encode($output, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES);
+        }
+
+    }
+
+
+    /**
+     * Imports settings and data.
+     * 
+     * ## OPTIONS
+     * 
+     * <what>
+     * : The name of the person to greet.
+     * 
+     * ## EXAMPLES
+     * 
+     *     wp amt import settings
+     *     wp amt import postdata
+     *     wp amt import userdata
+     *
+     * @synopsis <what>
+     */
+    function import( $args, $assoc_args ) {
+        list( $what ) = $args;
+
+        if ( ! in_array($what, array('settings', 'postdata', 'userdata')) ) {
+            WP_CLI::error( 'Invalid argument: ' . $what . ' (valid: settings|postdata|userdata)' );
+        }
+
+        // Import AMT settings
+        if ( $what == 'settings' ) {
+            $data = json_decode( file_get_contents('php://stdin') );
+            if ( empty($data) || ! is_array($data) ) {
+                WP_CLI::error( 'No data found.' );
+            }
+            // Since all other options might come and go, we only check for the 'settings_version' key.
+            if ( ! array_key_exists('settings_version', $data) ) {
+                WP_CLI::error('Invalid data: not option data');
+            }
+            //var_dump( $options );
+            update_option("add_meta_tags_opts", $data);
+            amt_plugin_upgrade();
+        }
+
+        // Import AMT post custom fields
+        elseif ( $what == 'postdata' ) {
+            $data = json_decode( file_get_contents('php://stdin') );
+            if ( empty($data) || ! is_array($data) || empty($data[0]) ) {
+                WP_CLI::error( 'No data found.' );
+            }
+            $amt_post_fields = amt_get_post_custom_field_names();
+            foreach ( $data[0] as $post_meta_info ) {
+                // Format: array( <id>, <field_name>, <field_value> )
+                if ( ! is_array($post_meta_info) || count($post_meta_info) != 3 || ! in_array( $post_meta_info[1], $amt_post_fields) || ! is_numeric($post_meta_info[0] ) ) {
+                    WP_CLI::error('Invalid data: not post custom field data');
+                }
+                update_post_meta( $post_meta_info[0], $post_meta_info[1], $post_meta_info[2] );
+            }
+
+            WP_CLI::success( 'Add-Meta-Tags post data was imported successfully.' );
+        }
+
+        // Import AMT contact infos
+        elseif ( $what == 'userdata' ) {
+            $data = json_decode( file_get_contents('php://stdin') );
+            if ( empty($data) || ! is_array($data) || empty($data[0]) ) {
+                WP_CLI::error( 'No data found.' );
+            }
+            $amt_user_fields = amt_get_user_contactinfo_field_names();
+            foreach ( $data[0] as $user_meta_info ) {
+                // Format: array( <id>, <field_name>, <field_value> )
+                if ( ! is_array($user_meta_info) || count($user_meta_info) != 3 || ! in_array( $user_meta_info[1], $amt_user_fields) || ! is_numeric($user_meta_info[0] ) ) {
+                    WP_CLI::error('Invalid data: not user contact infos');
+                }
+                update_user_meta( $user_meta_info[0], $user_meta_info[1], $user_meta_info[2] );
+            }
+
+            WP_CLI::success( 'Add-Meta-Tags user data was imported successfully.' );
+        }
+
+    }
+
+
+    /**
+     * Cleans settings and data.
+     * 
+     * ## OPTIONS
+     * 
+     * <what>
+     * : Choose what kind of data you would like to delete.
+     * 
+     * ## EXAMPLES
+     * 
+     *     wp amt clean all
+     *     wp amt clean settings
+     *     wp amt clean postdata
+     *     wp amt clean userdata
+     *
+     * @synopsis <all|settings|postdata|userdata>
+     */
+    function clean( $args, $assoc_args ) {
+        list( $what ) = $args;
 
         if ( ! in_array($what, array('all', 'settings', 'postdata', 'userdata')) ) {
             WP_CLI::error( 'Invalid argument: ' . $what . ' (valid: all|settings|postdata|userdata)' );
-        } elseif ( ! empty($assoc_args['blog-id']) && ! empty($assoc_args['blog-id']) ) {
-            WP_CLI::error( 'Mutually exclusive arguments: --blog-id, --blog-domain' );
-        } elseif ( empty($assoc_args['blog-id']) && empty($assoc_args['blog-id']) ) {
-            WP_CLI::error( 'At least one required: --blog-id, --blog-domain' );
         }
 
-WP_CLI::success( "Operation complete." );
-
-        if ( is_multisite() ) {
-            $blog_list = get_blog_list( 0, 'all' );
-        } else {
-            $blog_list   = array();
-            $blog_list[] = array( 'blog_id' => 1 );
+        // Confirmation
+        WP_CLI::line( ' ' );
+        WP_CLI::line( 'This commands deletes Add-Meta-Tags data from the database.' );
+        WP_CLI::line( 'This action is final and cannot be undone.' );
+        WP_CLI::line( ' ' );
+        echo 'Are you sure you want to do this?  Type \'yes\' to continue: ';
+        $handle = fopen( 'php://stdin', 'r' );
+        $choice = fgets($handle);
+        fclose($handle);
+        if ( trim($choice) != 'yes' ) {
+            WP_CLI::line( 'Aborting...' );
+            exit;
+        }
+        WP_CLI::line( ' ' );
+        WP_CLI::line( 'Proceeding with ' . $what . ' cleanup...' );
+        
+        // Delete AMT settings
+        if ( $what == 'settings' || $what == 'all' ) {
+            delete_option('add_meta_tags_opts');
+            WP_CLI::line( 'Deleted settings.' );
         }
 
-        foreach ( $blog_list as $blog ) {
-            if ( is_multisite() ) {
-                switch_to_blog( $blog['blog_id'] );
+        // Delete AMT post custom fields
+        elseif ( $what == 'postdata' || $what == 'all' ) {
+            $qr_args = array(
+                'numberposts'       => -1,
+                'post_type'         => 'any',
+                'post_status'       => 'any',
+                'orderby'           => 'id',
+                'order'             => 'ASC',
+                'suppress_filters'  => true,
+            );
+            $posts_arr = get_posts( $qr_args );
+            $amt_post_fields = amt_get_post_custom_field_names();
+            foreach ( $posts_arr as $post ) {
+                foreach ( $amt_post_fields as $amt_post_field ) {
+                    delete_post_meta( $post->ID, $amt_post_field );
+                }
             }
-
-            $options = get_option("add_meta_tags_opts");
-            //var_dump( $options );
-            echo json_encode($options, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES);
-            //$plugin_info = get_plugin_data( plugin_dir_path( __FILE__ ) . 'add-meta-tags.php', $markup = true, $translate = true );
-            //WP_CLI::line( 'Upgrading settings of ' . get_bloginfo('name') . ' - ' . $blog['blog_id'] );
-            //amt_plugin_upgrade();
-
-            if ( is_multisite() ) {
-                restore_current_blog();
-            }
+            WP_CLI::line( 'Deleted post custom fields.' );
         }
 
-        // get_plugin_data( $plugin_file, $markup = true, $translate = true )
-        //$plugin info = get_plugin_data( AMT_PLUGIN_DIR . 'add-meta-tags.php', $markup = true, $translate = true );
-        // WP_CLI::line( ' ' );
-        // WP_CLI::line( count( $field_groups ) . ' field groups found for blog_id ' . $blog['blog_id'] );
-
-        // Print a success message
-        //WP_CLI::success( "Operation complete." );
+        // Import AMT contact infos
+        elseif ( $what == 'userdata' || $what == 'all' ) {
+            $qr_args = array(
+                'orderby'      => 'login',
+                'order'        => 'ASC',
+                'fields'       => 'all',
+            );
+            $users_arr = get_users( $qr_args );
+            $amt_user_fields = amt_get_user_contactinfo_field_names();
+            foreach ( $users_arr as $user ) {
+                foreach ( $amt_user_fields as $amt_user_field ) {
+                    delete_user_meta( $user->ID, $amt_user_field );
+                }
+            }
+            WP_CLI::line( 'Deleted user contact info fields.' );
+        }
+        WP_CLI::success( 'Data clean up complete.' );
     }
 
 }
