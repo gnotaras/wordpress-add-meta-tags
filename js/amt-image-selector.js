61aac93d150fda75257c012c5e35463a260d88af
George Notaras <gnot@g-loaded.eu>
2016-03-28 04:15:37 +0300

    Updated the media selector script.

--- js/amt-image-selector.js
+++ js/amt-image-selector.js
@@ -11,14 +11,20 @@ jQuery( function($) {
 
     // Set all variables to be used in scope
     var frame,
-        metaBox = $('.form-table'), // Your meta box id here
-        addImgLink = metaBox.find('#amt_image_selector_button'),
-        //delImgLink = metaBox.find( '.delete-custom-img'),
-        //imgContainer = metaBox.find( '.amt-preview-image'),
-        imgIdInput = metaBox.find( '#default_image_url' );
-  
-    // ADD IMAGE LINK
-    addImgLink.on( 'click', function( event ){
+        //metaBox = $('.form-table'), // Your meta box id here
+        metaBox = $('#add-meta-tags-settings, .add-meta-tags-setting'), // Your meta box id here
+        //metaBox = $('.form-field'), // Your meta box id here
+        //metaBox = $('#add-meta-tags-settings'), // Your meta box id here
+        //metaBox = $('#add-meta-tags-settings, .add-meta-tags-setting'), // Your meta box id here
+        // Default Image
+        buttonSelectDefaultImage = metaBox.find('#amt-default-image-selector-button'),
+        inputDefaultImage = metaBox.find( '#default_image_url' );
+        // Global Image Override
+        buttonSelectImage = metaBox.find('#amt-image-selector-button'),
+        inputImage = metaBox.find( '#amt_custom_image_url' );
+
+    // Default Image
+    buttonSelectDefaultImage.on( 'click', function( event ){
     
         event.preventDefault();
         
@@ -30,7 +36,7 @@ jQuery( function($) {
         
         // Create a new media frame
         frame = wp.media({
-            title: 'Select or Upload Media Of Your Chosen Persuasion',
+            title: 'Select or upload the default image',
             button: {
                 text: 'Use this media'
             },
@@ -44,24 +50,50 @@ jQuery( function($) {
             // Get media attachment details from the frame state
             var attachment = frame.state().get('selection').first().toJSON();
 
-            // Send the attachment URL to our custom image input field.
-            //imgContainer.append( '<img src="'+attachment.url+'" alt="" style="max-width:100%;"/>' );
-            //imgContainer.append( '<img src="'+attachment.sizes.thumbnail.url+'" alt="" style="max-width:100%;"/>' );
-            //imgContainer.attr('src', attachment.sizes.thumbnail.url);
-
             // Send the attachment id to our hidden input
-            imgIdInput.val( attachment.id );
-
-      // Hide the add image link
-      //addImgLink.addClass( 'hidden' );
-
-      // Unhide the remove image link
-      //delImgLink.removeClass( 'hidden' );
+            inputDefaultImage.val( attachment.id );
 
         });
 
         // Finally, open the modal on click
         frame.open();
-  });
+    });
+
+
+    // Global Image Override
+    buttonSelectImage.on( 'click', function( event ){
+    
+        event.preventDefault();
+        
+        // If the media frame already exists, reopen it.
+        if ( frame ) {
+            frame.open();
+            return;
+        }
+        
+        // Create a new media frame
+        frame = wp.media({
+            title: 'Select or upload an image',
+            button: {
+                text: 'Use this media'
+            },
+            multiple: false  // Set to true to allow multiple files to be selected
+        });
+
+        
+        // When an image is selected in the media frame...
+        frame.on( 'select', function() {
+          
+            // Get media attachment details from the frame state
+            var attachment = frame.state().get('selection').first().toJSON();
+
+            // Send the attachment id to our hidden input
+            inputImage.val( attachment.id );
+
+        });
+
+        // Finally, open the modal on click
+        frame.open();
+    });
 
 });
